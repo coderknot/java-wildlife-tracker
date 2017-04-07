389326fed6f53a290ab353de89494bddf97d21ca
Chris Carr <coder.knot@gmail.com>
2017-04-07 09:24:04 -0700

    update code ordering for ease of testing and readability

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -12,10 +12,12 @@ public class DatabaseRule extends ExternalResource {
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
       String deleteAnimalsQuery = "DELETE FROM animals *;";
-      String deleteEndangeredAnimalsQuery = "DELETE FROM endangered_animals *;";
-      String deleteSightingsQuery = "DELETE FROM sightings *;";
       con.createQuery(deleteAnimalsQuery).executeUpdate();
+
+      String deleteEndangeredAnimalsQuery = "DELETE FROM endangered_animals *;";
       con.createQuery(deleteEndangeredAnimalsQuery).executeUpdate();
+
+      String deleteSightingsQuery = "DELETE FROM sightings *;";
       con.createQuery(deleteSightingsQuery).executeUpdate();
     }
   }
