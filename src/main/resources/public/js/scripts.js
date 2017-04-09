bd4ab0fad5d8ded10bd263948e743301c0dc9795
Chris Carr <coder.knot@gmail.com>
2017-04-09 02:31:59 -0700

    add custom js for showing or hiding additional options for add sightings

--- src/main/resources/public/js/scripts.js
+++ src/main/resources/public/js/scripts.js
@@ -10,4 +10,16 @@ $(document).ready(function() {
 
   });
 
+  $("input:radio[name='add-sighting-type']").click(function() {
+
+    if($(this).attr('id') == 'add-sighting-type-endangered') {
+      $('#add-sighting-non').hide();
+      $('#add-sighting-endangered').show();
+    } else {
+      $('#add-sighting-endangered').hide();
+      $('#add-sighting-non').show();
+    }
+
+  });
+
 });
